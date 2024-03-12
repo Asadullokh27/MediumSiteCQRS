571ce94c6f570af723f03b5ee5fc4dd153e0af7e
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-12 08:32:33 +0500

    DeleteUserCommand added

---
+++ MediumSite.Application/UseCases/Commands/DeleteUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediumSite.Application.UseCases.Commands
+{
+    public class DeleteUserCommand:IRequest
+    {
+
+        public Guid Id { get; set; }
+
+    }
+}
