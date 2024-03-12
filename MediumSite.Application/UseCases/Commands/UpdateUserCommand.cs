156d86d34c6c5335041d31555312da8ec2a76200
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-12 08:30:27 +0500

    updateUserCommand added

---
+++ MediumSite.Application/UseCases/Commands/UpdateUserCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MediumSite.Domain.DTOs;
+using MediumSite.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediumSite.Application.UseCases.Commands
+{
+    public class UpdateUserCommand : UserDTO, IRequest<User>
+    {
+
+        public Guid Id { get; set; }
+
+    }
+}
